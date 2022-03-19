9937829569b5a2b74ea2c067fc29614fe2b3e0b2
Thomas Wechdorn <thomas.wechdorn@allianz.at>
2022-03-19 09:49:19 +0100

    add first prototype of rhythm mechanic

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public PressableObject pressableObject;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (pressableObject != null)
+            {
+                pressableObject.press();
+            }
+            else
+            {
+                Debug.Log("remove Life");
+            }
+        }
+    }
+}
+
+ 
